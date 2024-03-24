593601d3a8b7f4a1ec22d38e783d04ee28a65eb3
lasha-codes <lashagiorgi420@gmail.com>
2024-03-24 16:28:30 -0700

    learning how to create tables update them and also delete them pretty cool

--- db.sql
+++ db.sql
@@ -6,8 +6,19 @@
 -- list all tables \d
 
 CREATE TABLE products (
-    id INT,
-    name VARCHAR(50),
-    price INT,
-    on_sale boolean
+  id INT,
+  name VARCHAR(50),
+  price INT,
+  on_sale boolean
+);
+
+ALTER TABLE products ADD COLUMN featured boolean;
+ALTER TABLE products DROP COLUMN featured;
+
+
+CREATE TABLE restaurants (
+  id INT,
+  name VARCHAR(50),
+  location VARCHAR(50),
+  price_range INT
 );
