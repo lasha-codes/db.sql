45b96886f1ac1ce8ba122898b252871568bab9b2
lasha-codes <lashagiorgi420@gmail.com>
2024-03-24 16:12:47 -0700

    learning to work on sql based databases specifically mySql and postgreSql

---
+++ db.sql
@@ -0,0 +1,13 @@
+-- for help   \?
+-- list database \l
+
+-- Create database CREATE DATABASE database_name;
+
+-- list all tables \d
+
+CREATE TABLE products (
+    id INT,
+    name VARCHAR(50),
+    price INT,
+    on_sale boolean
+);
