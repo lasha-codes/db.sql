57e6a68ebfc07df24f7cefa4b5bddf1f3b1edc45
lasha-codes <lashagiorgi420@gmail.com>
2024-03-24 17:22:10 -0700

    learned new NOT NULL and unique auto increment BIGSERIAL type

--- db.sql
+++ db.sql
@@ -5,6 +5,8 @@
 
 -- list all tables \d
 
+-- writing down important commands
+
 CREATE TABLE products (
   id INT,
   name VARCHAR(50),
@@ -15,12 +17,11 @@ CREATE TABLE products (
 ALTER TABLE products ADD COLUMN featured boolean;
 ALTER TABLE products DROP COLUMN featured;
 
-
 CREATE TABLE restaurants (
-  id INT,
-  name VARCHAR(50),
-  location VARCHAR(50),
-  price_range INT
+  id BIGSERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL
 );
 
 INSERT INTO restaurants (id, name, location, price_range) values (123, 'McDonald', 'new york', 3);
