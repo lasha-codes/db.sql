23e49bf0467124a377a222d353ee95724876ff7a
lasha-codes <lashagiorgi420@gmail.com>
2024-03-25 04:02:24 -0700

    learning strict types

--- db.sql
+++ db.sql
@@ -18,12 +18,15 @@ ALTER TABLE products ADD COLUMN featured boolean;
 ALTER TABLE products DROP COLUMN featured;
 
 CREATE TABLE restaurants (
-  id BIGSERIAL NOT NULL,
+  id BIGSERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   location VARCHAR(50) NOT NULL,
-  price_range INT NOT NULL
+  price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
 );
 
 INSERT INTO restaurants (id, name, location, price_range) values (123, 'McDonald', 'new york', 3);
 INSERT INTO restaurants (price_range) values(12);
 SELECT * FROM restaurants;
+DROP TABLE restaurants;
+
+-- stopLine - 1:02:54
